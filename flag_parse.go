98cea9529cf277035498fe9738dace2ba8693a2e
soarpenguin <soarpenguin@gmail.com>
2015-11-01 23:00:02 +0800

    add go flag parse usage.

---
+++ flag_parse.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+    "flag"
+)
+
+func main(){
+
+    data_path := flag.String("D", "~/sample/", "DB data path")
+    log_file := flag.String("l", "~/sample.log", "log file")
+    nowait_flag := flag.Bool("W", false, "do not wait until operation completes")
+
+    flag.Parse()
+
+    var cmd string = flag.Arg(0);
+
+    fmt.Printf("action   : %s\n", cmd)
+    fmt.Printf("data path: %s\n", *data_path)
+    fmt.Printf("log file : %s\n", *log_file)
+    fmt.Printf("nowait     : %v\n", *nowait_flag)
+
+    fmt.Printf("-------------------------------------------------------\n")
+
+    fmt.Printf("there are %d non-flag input param\n", flag.NArg())
+    for i, param := range flag.Args(){
+        fmt.Printf("#%d    :%s\n", i, param)
+    }
+}
