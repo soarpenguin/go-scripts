26f50000809a45f4faea7705538be4c15bc88eb0
soarpenguin <soarpenguin@gmail.com>
2015-12-05 10:06:55 +0800

    lineStatistic.go - cmdline utility for statistic line info from file or stdin

---
+++ lineStatistic.go
@@ -0,0 +1,41 @@
+package main
+
+// lineStatistic prints the count and text of lines that appear more than once
+// in the input.  It reads from stdin or from a list of named files.
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+
+	if len(files) == 0 {
+		countLines(os.Stdin, counts)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "lineStatistic: %v\n", err)
+				continue
+			}
+			countLines(f, counts)
+			f.Close()
+		}
+	}
+
+	for line, n := range counts {
+		fmt.Printf("%d\t%s\n", n, line)
+	}
+}
+
+func countLines(f *os.File, counts map[string]int) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		counts[input.Text()]++
+	}
+	// NOTE: ignoring potential errors from input.Err()
+}
