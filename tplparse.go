b5fbb729967ac30b839bca3554c6735fe60aa886
soarpenguin <soarpenguin@gmail.com>
2016-01-27 10:54:50 +0800

    add output parameter.

--- tplparse.go
+++ tplparse.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"math/rand"
 	"os"
 	"path/filepath"
 	"regexp"
 	"strings"
 	"text/template"
+	"time"
 
 	flag "github.com/docker/docker/pkg/mflag"
 )
@@ -21,6 +23,7 @@ const (
 
 var (
 	help                bool
+	output              string
 	tplfile, appName    string
 	consulIp, virtualIp string
 )
@@ -120,11 +123,73 @@ func checkAppName(appName string) (appname string, err error) {
 	return appname, nil
 }
 
+// generate rand string
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const (
+	letterIdxBits = 6                    // 6 bits to represent a letter index
+	letterIdxMask = 1<<letterIdxBits - 1 // All 1-bits, as many as letterIdxBits
+	letterIdxMax  = 63 / letterIdxBits   // # of letter indices fitting in 63 bits
+)
+
+func RandStringBytesMaskImprSrc(n int) string {
+	var src = rand.NewSource(time.Now().UnixNano())
+
+	b := make([]byte, n)
+	// A src.Int63() generates 63 random bits, enough for letterIdxMax characters!
+	for i, cache, remain := n-1, src.Int63(), letterIdxMax; i >= 0; {
+		if remain == 0 {
+			cache, remain = src.Int63(), letterIdxMax
+		}
+		if idx := int(cache & letterIdxMask); idx < len(letterBytes) {
+			b[i] = letterBytes[idx]
+			i--
+		}
+		cache >>= letterIdxBits
+		remain--
+	}
+
+	return string(b)
+}
+
+func genConfigFile(tplfile string, app Application, output, appname string) {
+	output = strings.TrimSpace(output)
+
+	if output == "" || output == "appname.conf" {
+		output = appname + ".conf"
+	}
+
+	if _, err := isExists(output); err == nil {
+		log.Printf("[WARN] %s is existed, will redirect to tmp file!!!!", output)
+		output = fmt.Sprintf("%s.%s", output, RandStringBytesMaskImprSrc(6))
+	}
+
+	t, err := template.ParseFiles(tplfile)
+	if err != nil {
+		log.Fatalf("Template parse failed:%s\n", err)
+	}
+
+	ofd, err := os.OpenFile(output, os.O_WRONLY|os.O_CREATE, 0666)
+	if err != nil {
+		log.Printf("[WARN] Open %s failed: %s\n", output, err)
+		ofd = os.Stdout
+	} else {
+		defer ofd.Close()
+	}
+
+	//err = t.Execute(os.Stdout, app)
+	err = t.Execute(ofd, app)
+	if err != nil {
+		log.Fatalf("Template execute failed:%s\n", err)
+	}
+	log.Printf("[NOTICE] please check content in %s", output)
+}
+
 func init() {
 	flag.StringVar(&tplfile, []string{"t", "-template"}, "server.conf.template", "Template file name for produce config file.")
 	flag.StringVar(&appName, []string{"-app"}, "appname", "App name for upstream/logfile name/conf name/consul key.")
 	flag.StringVar(&consulIp, []string{"c", "-consul"}, "10.10.10.10:8500", "Consul server 'url:port' for get upstream info.")
 	flag.StringVar(&virtualIp, []string{"v", "-virtualip"}, "0.0.0.0,0.0.0.1", "Virtual IP list for this app.")
+	flag.StringVar(&output, []string{"o", "-output"}, "appname.conf", "Config file name for save config file.")
 	flag.BoolVar(&help, []string{"h", "-help"}, false, "Display the help")
 	flag.Parse()
 }
@@ -170,19 +235,12 @@ func main() {
 		log.Fatalf("%s\n", err)
 	}
 
+	// new a application struct.
 	app := Application{
 		AppName:  appName,
 		ConsulIp: consulIp,
 		Servers:  vips,
 	}
-
-	t, err := template.ParseFiles(tplfile)
-	if err != nil {
-		log.Fatalf("Template parse failed:%s\n", err)
-	}
-
-	err = t.Execute(os.Stdout, app)
-	if err != nil {
-		log.Fatalf("Template execute failed:%s\n", err)
-	}
+	// generate final config file.
+	genConfigFile(tplfile, app, output, appName)
 }
