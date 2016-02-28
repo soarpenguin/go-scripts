b40a1d29d8cb25c52925cc5019b72e6813220243
soarpenguin <soarpenguin@gmail.com>
2016-02-28 16:19:52 +0800

    add usage for golang recover panic flow can reach or not.

--- defer_revover.go
+++ defer_revover.go
@@ -27,3 +27,48 @@ func g(i int) {
 	fmt.Println("Printing in g", i)
 	g(i + 1)
 }
+
+//-------------------------------------------------
+//
+//func explode() {
+//	// Cause a panic.
+//	panic("WRONG")
+//}
+//
+//func main() {
+//	// Handle errors in defer func with recover.
+//	defer func() {
+//		if err := recover(); err != nil {
+//			// Handle our error.
+//			fmt.Println("FIX")
+//			fmt.Println("ERR", err)
+//		}
+//	}()
+//	// This causes an error.
+//	explode()
+//	fmt.Println("reach here ") // 达不到
+//}
+//
+
+//func explode() {
+//	// Cause a panic.
+//	panic("WRONG")
+//
+//}
+//
+//func throwPanic(f func()) {
+//	defer func() {
+//		if err := recover(); err != nil {
+//			// Handle our error.
+//			fmt.Println("FIX")
+//			fmt.Println("ERR", err)
+//		}
+//	}()
+//	f()
+//	fmt.Println(" finish")
+//}
+//
+//func main() {
+//	throwPanic(explode)
+//	fmt.Println("reach here ") //可达
+//}
