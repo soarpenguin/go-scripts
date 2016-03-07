ccbf87f1b54edcae401d463d62e48ac03934e6e6
soarpenguin <soarpenguin@gmail.com>
2016-03-07 22:32:26 +0800

    format code.

--- channels.go
+++ channels.go
@@ -1,29 +1,31 @@
 package main
 
-import "time"
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
-    timeChan := time.NewTimer(time.Second).C
-    
-    tickChan := time.NewTicker(time.Millisecond * 400).C
-    
-    doneChan := make(chan bool)
-    go func() {
-        time.Sleep(time.Second * 2)
-        doneChan <- true
-    }()
-    
-    for {
-        select {
-        case <- timeChan:
-            fmt.Println("Timer expired")
-        case <- tickChan:
-            fmt.Println("Ticker ticked")
-        case <- doneChan:
-            fmt.Println("Done")
-            return
-      }
-    }
-}
+	// once running
+	timeChan := time.NewTimer(time.Second).C
 
+	tickChan := time.NewTicker(time.Millisecond * 400).C
+
+	doneChan := make(chan bool)
+	go func() {
+		time.Sleep(time.Second * 2)
+		doneChan <- true
+	}()
+
+	for {
+		select {
+		case <-timeChan:
+			fmt.Println("Timer expired")
+		case <-tickChan:
+			fmt.Println("Ticker ticked")
+		case <-doneChan:
+			fmt.Println("Done")
+			return
+		}
+	}
+}
