8557aabcfd9bcdb4a3a42361ad3e4fe9d9d39fcf
soarpenguin <soarpenguin@gmail.com>
2016-08-30 23:42:18 +0800

    add simple golang dns lib usage.

---
+++ dnsUsage.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"github.com/miekg/dns"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	if len(os.Args) <= 1 {
+		log.Printf("Usage: %s domain", os.Args[0])
+		os.Exit(1)
+	}
+
+	config, _ := dns.ClientConfigFromFile("/etc/resolv.conf")
+	c := new(dns.Client)
+
+	m := new(dns.Msg)
+	m.SetQuestion(dns.Fqdn(os.Args[1]), dns.TypeMX)
+	m.RecursionDesired = true
+
+	r, _, err := c.Exchange(m, net.JoinHostPort(config.Servers[0], config.Port))
+	if r == nil {
+		log.Fatalf("*** error: %s\n", err.Error())
+	}
+
+	if r.Rcode != dns.RcodeSuccess {
+		log.Fatalf(" *** invalid answer name %s after MX query for %s\n", os.Args[1], os.Args[1])
+	}
+	// Stuff must be in the answer section
+	for _, a := range r.Answer {
+		fmt.Printf("%v\n", a)
+	}
+}
