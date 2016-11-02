d6f4d01ded56b366ef41d63e8eab98460dc56b4f
soarpenguin <soarpenguin@gmail.com>
2016-11-02 22:10:34 +0800

    add docker client lib usage.

---
+++ dockerManage.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+
+	dockerapi "github.com/fsouza/go-dockerclient"
+)
+
+func assert(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	dclient, err := dockerapi.NewClient("tcp://127.0.0.1:2375")
+	assert(err)
+
+	if containers, err := dclient.ListContainers(dockerapi.ListContainersOptions{All: true}); err != nil {
+		log.Fatalf("Get container error: %v", err)
+	} else {
+		for _, container := range containers {
+			if c, err := dclient.InspectContainer(container.ID); err != nil {
+				log.Printf("%s: %v\n", container.ID, err)
+			} else {
+				if c.State.Running {
+					continue
+				} else {
+					log.Println(c.ID)
+				}
+			}
+		}
+	}
+}
