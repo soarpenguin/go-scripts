841137049e4bb5dc3d13d9d0d13303c02b3dfb59
soarpenguin <soarpenguin@gmail.com>
2017-01-05 23:57:36 +0800

    add utility func sptr/iptr/u32ptr/fmptr.

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"os"
 )
 
 type ByteSize float64
@@ -17,6 +18,11 @@ const (
 	ZB
 	YB
 )
+
+func sPtr(s string) *string      { return &s }
+func iPtr(i int64) *int64        { return &i }
+func u32Ptr(i int64) *uint32     { u := uint32(i); return &u }
+func fmPtr(i int64) *os.FileMode { fm := os.FileMode(i); return &fm }
 
 func (b ByteSize) String() string {
 	switch {
